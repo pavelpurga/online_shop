02e4b11526c88101a3c1cf616567ed1bbd0ef478
Pablo.ovrf <pashapetrokas@gmail.com>
2023-03-20 00:12:37 +0300

    client part

---
+++ src/components/DeviceList.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react';
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {Form} from "react-bootstrap";
+import DeviceItem from "./DeviceItem";
+
+const DeviceList = observer(() => {
+    const {device} = useContext(Context)
+    return (
+        <Form className="d-flex">
+            {device.devices.map(device =>
+                <DeviceItem key={device.id} device={device}/>
+            )}
+        </Form>
+    );
+});
+export default DeviceList;
---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React, {useContext} from 'react';
+import {Context} from "../index";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {NavLink} from "react-router-dom";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
+import {Button, Container} from "react-bootstrap";
+import {observer} from "mobx-react-lite";
+import {useNavigate} from "react-router-dom";
+
+const NavBar = observer(() => {
+    const {user}=useContext(Context)
+    const navigate=useNavigate()
+    const logOut=()=>{
+        user.setUser()
+        user.setIsAuth(false)
+    }
+        return (
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                    <NavLink style={{color:'white'}} to={SHOP_ROUTE}>Pablostore</NavLink>
+                    {user._isAuth ?
+                        <Nav className="ml-auto" style={{color:'white'}}>
+                            <Button
+                                variant={"outline-light"}
+                                onClick={()=>navigate(ADMIN_ROUTE)}>
+                                Админ панель
+                            </Button>
+                            <Button
+                                variant={"outline-light"}
+                                onClick={()=>logOut()}
+                                className="ml-2">
+                                Выйти
+                            </Button>
+                        </Nav>
+                        :
+                        <Nav className="ml-auto" style={{color:'white'}}>
+                            <Button variant={"outline-light"} onClick={()=>navigate(LOGIN_ROUTE)}>Авторизация</Button>
+                        </Nav>
+                    }
+                    </Container>
+                </Navbar>
+    );
+});
+
+export default NavBar;
